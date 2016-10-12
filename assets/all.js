b86b570427558bbcf30c69ba1ac92bdd87441326
levibrooke <levi@levibrooke.com>
2016-10-11 18:50:56 -0700

    Adding token for instafeed

--- assets/all.js
+++ assets/all.js
@@ -10,9 +10,9 @@
 // instafeed
 var feed = new Instafeed({
     get: 'user',
-    userId: '31176171',
-    accessToken: '31176171.fea787d.53fdae91a765404e8aa92c654757abc0',
-    clientId: '298e54c29bec4729910c1054558ed11b',
+    userId: '267315238',
+    accessToken: '267315238.d11dd2f.2e085b492f4a4a3795d16a6c369e1d43',
+    clientId: 'd11dd2fcf06b412ba40b519f224e3d04',
     resolution: 'standard_resolution',
     limit: '12',
     template: '<div class="col-md-2" id="single"><a href="{{link}}" target="_blank"><img class="img-responsive" src="{{image}}" /><span class="likes"><i class="fa fa-heart-o"></i>{{likes}}</span></a></div>',
